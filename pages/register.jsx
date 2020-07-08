c82590b690a46ae88a05e86bad14150b950cc056
Olamide <aboyejiolamide15@gmail.com>
2020-07-08 13:00:15 +0100

    fix typo in register page

--- pages/register.jsx
+++ pages/register.jsx
@@ -7,10 +7,10 @@ export default () => {
     <Layout>
       <Head>
         <title>Create Account on Ojaa</title>
-        <meta name="description" content="Create and account on Ojaafoods"/>
-        <meta property="og:description" content="Create and account on Ojaafoods"/>
+        <meta name="description" content="Create an account on Ojaafoods"/>
+        <meta property="og:description" content="Create an account on Ojaafoods"/>
         <meta name="keywords" content="Login to ojaa ojaalogin register create account"/>
-        <meta name="twitter:description" content="Create and account on Ojaafoods" />
+        <meta name="twitter:description" content="Create an account on Ojaafoods" />
       </Head>
       <Register />
     </Layout>
